05f844d2841b8a2123e2cd020906a4139876e22b
Ivesf1m <ivesfmoura@gmail.com>
2016-07-27 22:14:12 -0300

    Added getPointAtLambda function in Ray class.

--- RayTracer/RayTracer/Ray.h
+++ RayTracer/RayTracer/Ray.h
@@ -12,11 +12,16 @@ public:
 	Ray(vec3& origin, vec3& direction);
 	~Ray();
 
+	//Getters and setters
 	vec3& getDirection();
 	void setDirection(const vec3& direction);
 
 	vec3& getOrigin();
 	void setOrigin(const vec3& origin);
+
+	//Get point at a specified lambda.
+	//Follows the formula y = origin + lambda * direction
+	vec3& getPointAtLambda(float lambda);
 
 	void printRayInfo();
 
--- RayTracer/RayTracer/main.cpp
+++ RayTracer/RayTracer/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char** argv)
 	vec3 direction(1.0f, 0.0f, 0.0f);
 	Ray r(origin, direction);
 	r.printRayInfo();
+	vec3 point = r.getPointAtLambda(2.0f);
+	std::cout << point.x << "\t" << point.y << "\t" << point.z << std::endl;
 	getchar();
 	return 0;
 }
